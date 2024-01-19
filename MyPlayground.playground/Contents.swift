701a43e0eb2176d1f671818fd1f38dd7a5da9711
kzb0125 <kzb0125@auburn.edu>
2024-01-19 01:50:31 -0800

    func monthlyPayment cleaned up and commented

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,22 +1,27 @@
 import UIKit
 
+// monthly payment calculation
+// input:   principal as Int
+//          number of payments (month) as Int
+//          interest rate as Float
+// output:  monthly payment as Float
 func monthlyPayment(_ principal: Int, _ numOfPayment: Int, _ interestRate: Float) -> Float{
+    // rate is casted to a decimal type for use in the pow(x:Decimal , y:Int) function
     let rate = Decimal(Double(interestRate))/1200
+    // rPower created for use in amorization formula
     let rPower = pow(1 + rate, numOfPayment)
+    // monthly payment calculation based on a monthly amortization formula (datatype: Decimal)
     let monthlyPayment = Decimal(principal) * rate * rPower / (rPower - 1)
     
-    print("principal: \(principal)",type(of: principal))
-    print("numOfPayment: \(numOfPayment)", type(of: numOfPayment))
-    print("rate: \(rate)", type(of: rate))
-    print("monthlyPayment: \(monthlyPayment)", type(of: monthlyPayment))
+    // Decimal to String cast
+    let monthlyPaymentString = NSDecimalNumber(decimal: monthlyPayment).stringValue
+    // String to Float cast
+    var monthlyPaymentFloat = (monthlyPaymentString as NSString).floatValue
+    // round monthly payment to the hundredth place to represent cents.
+    monthlyPaymentFloat = (monthlyPaymentFloat*100).rounded()/100
     
-    //print("print (unrounded) \(testPrint1)", type(of: testPrint1))
-    let testPrint2 = NSDecimalNumber(decimal: monthlyPayment).stringValue
-    print("print (unrounded) \(testPrint2)", type(of: testPrint2))
-    var myFloat = (testPrint2 as NSString).floatValue
-    print("myFloat \(myFloat)", type(of: myFloat))
-    print((myFloat*100).rounded()/100)
-    return 2.1
+    return monthlyPaymentFloat
 }
 
 print(monthlyPayment(20000, 2, 4.4))
+print(monthlyPayment(150000, 360, 5))
