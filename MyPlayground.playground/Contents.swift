698f3a92c25dc188aa545e98b7b3db870b9b0c59
kzb0125 <kzb0125@auburn.edu>
2024-01-19 01:28:29 -0800

    func monthlyPayment created - ver.1.0

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,22 @@
 import UIKit
 
-var principal : Int?
-var numOfPayment : Int?
-var apr : Float?
+func monthlyPayment(_ principal: Int, _ numOfPayment: Int, _ interestRate: Float) -> Float{
+    let rate = Decimal(Double(interestRate))/1200
+    let rPower = pow(1 + rate, numOfPayment)
+    let monthlyPayment = Decimal(principal) * rate * rPower / (rPower - 1)
+    
+    print("principal: \(principal)",type(of: principal))
+    print("numOfPayment: \(numOfPayment)", type(of: numOfPayment))
+    print("rate: \(rate)", type(of: rate))
+    print("monthlyPayment: \(monthlyPayment)", type(of: monthlyPayment))
+    
+    //print("print (unrounded) \(testPrint1)", type(of: testPrint1))
+    let testPrint2 = NSDecimalNumber(decimal: monthlyPayment).stringValue
+    print("print (unrounded) \(testPrint2)", type(of: testPrint2))
+    var myFloat = (testPrint2 as NSString).floatValue
+    print("myFloat \(myFloat)", type(of: myFloat))
+    print((myFloat*100).rounded()/100)
+    return 2.1
+}
 
-
+print(monthlyPayment(20000, 2, 4.4))
