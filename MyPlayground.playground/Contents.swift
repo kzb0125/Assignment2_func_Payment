3c419f4ba7db7559a7032f253597a05e6582fa6a
kzb0125 <kzb0125@auburn.edu>
2024-01-19 14:36:08 -0800

    Added monthlyRate func

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,7 +7,7 @@ import UIKit
 // output:  monthly payment as Float
 func Payment(_ principal: Int, _ numOfPayment: Int, _ interestRate: Float) -> Float{
     // rate is casted to a decimal type for use in the pow(x:Decimal , y:Int) function
-    let rate = Decimal(Double(interestRate))/1200
+    let rate = Decimal(Double(interestRate))/100
     // rPower created for use in amorization formula
     let rPower = pow(1 + rate, numOfPayment)
     // monthly payment calculation based on a monthly amortization formula (datatype: Decimal)
@@ -23,5 +23,9 @@ func Payment(_ principal: Int, _ numOfPayment: Int, _ interestRate: Float) -> Fl
     return monthlyPaymentFloat
 }
 
-print(Payment(20000, 2, 4.4))
-print(Payment(150000, 360, 5))
+func monthlyRate(_ apr: Float) -> Float{
+    return apr/12
+}
+
+print(Payment(20000, 2, monthlyRate(4.4)))
+print(Payment(150000, 30, 5))
